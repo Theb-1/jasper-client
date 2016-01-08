f6cb69f34615a7b075eb70db73d0d8d60cb8364a
Theb-1 <tylechri@isu.edu>
2016-01-07 21:17:52 -0800

    Beep on unclear

--- plugins/speechhandler/unclear/unclear.py
+++ plugins/speechhandler/unclear/unclear.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from sys import maxint
-import random
+#import random
+from client import paths
 from client import plugin
 
 
@@ -20,16 +21,18 @@ class UnclearPlugin(plugin.SpeechHandlerPlugin):
         mic -- used to interact with the user (for both input and output)
         """
 
-        messages = [
-            self.gettext("I'm sorry, could you repeat that?"),
-            self.gettext("My apologies, could you try saying that again?"),
-            self.gettext("Say that again?"),
-            self.gettext("I beg your pardon?")
-        ]
-
-        message = random.choice(messages)
-
-        mic.say(message)
+        #messages = [
+        #    self.gettext("I'm sorry, could you repeat that?"),
+        #    self.gettext("My apologies, could you try saying that again?"),
+        #    self.gettext("Say that again?"),
+        #    self.gettext("I beg your pardon?")
+        #]
+        
+        #message = random.choice(messages)
+        
+        #mic.say(message)
+        
+        mic.play_file(paths.data('audio', 'beep_error.wav'))
 
     def is_valid(self, text):
         return True

ddb4b15f41aeac858d1eb86dcfbbc54734b1aace
Theb-1 <tylechri@isu.edu>
2016-01-07 23:30:26 -0800

    allow ability to specify ivona language vs only using master

--- plugins/tts/ivona-tts/ivona.py
+++ plugins/tts/ivona-tts/ivona.py
@@ -46,9 +46,15 @@ class IvonaTTSPlugin(plugin.TTSPlugin):
             sentence_break = None
 
         try:
-            language = self.profile['language']
+            language = self.profile['ivona-tts']['language']
         except KeyError:
-            language = 'en-US'
+            language = None
+        
+		if language is None:
+			try:
+				language = self.profile['language']
+			except KeyError:
+				language = 'en-US'
 
         self._pyvonavoice = pyvona.Voice(access_key, secret_key)
         self._pyvonavoice.codec = "mp3"
